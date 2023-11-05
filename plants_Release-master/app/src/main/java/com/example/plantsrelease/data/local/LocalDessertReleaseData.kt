9a46d27e6705c66d184c0dbf0d09e7c3dcb03cc7
Alejandro  David Arzola Saavedra <90756437+AlejandroDavidArzolaSaavedra@users.noreply.github.com>
2023-11-05 21:35:39 +0000

    Add files via upload

---
+++ plants_Release-master/app/src/main/java/com/example/plantsrelease/data/local/LocalDessertReleaseData.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.plantsrelease.data.local
+
+/*
+ * Set data for Android Release dessert name
+ * the list contain distinct sequential dessert name
+ */
+object LocalDessertReleaseData {
+    val dessertReleases = listOf(
+        "Aloe Vera",
+        "Bamboo",
+        "Cactus",
+        "Daisy",
+        "Eucalyptus",
+        "Fern",
+        "Geranium",
+        "Hibiscus",
+        "Ivy",
+        "Jasmine",
+        "Kale",
+        "Lavender",
+        "Mint",
+        "Narcissus",
+        "Orchid",
+        "Pansy",
+        "Quince Tree",
+        "Rose",
+        "Sunflower",
+        "Tulip",
+        "Umbrella Plant",
+        "Violet",
+        "Willow",
+        "Xerophyte",
+        "Yarrow",
+        "Zinnia"
+    )
+}
